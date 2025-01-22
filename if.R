3d6810eb7c345ec81bf278e34f44fc373b049d63
sivasakthi55 <sivasakthiu192007@gmail.com>
2025-01-22 13:05:56 +0530

    22/01/2025

---
+++ if.R
@@ -0,0 +1,26 @@
+# if statement
+
+a=47
+b=57
+{
+if(a>b)
+  print("a is greater than b")
+else 
+  print("b is greater than a")
+
+}
+
+a=45
+b=458
+{
+  if(a>b)
+    print("a is greater than b")
+  else if (a==b)
+    print("a is equal to b")
+  else
+    print("a is less than b ")
+}
+paste(" a values is :",a)
+
+
+
---
+++ nested if.R
@@ -0,0 +1,16 @@
+# Nested if 
+
+x=22
+{
+if(x>45)
+{
+  print("above 45")
+  
+  if(x>23)
+    print("above 23")
+  else
+    print("below 23")
+  }
+else
+  print("below 45")
+}
